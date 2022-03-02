84853b8f71c01a5e88feeff808c3c8becb29aa8c
ShaliniC255 <84853614+ShaliniC255@users.noreply.github.com>
2022-03-02 09:07:47 +0530

    added controllers/organisation.js

---
+++ controllers/organisation.js
@@ -0,0 +1,18 @@
+const organisationSchema = require('../models/organisation');
+
+
+module.exports.index = async (req, res) => {
+    const organisations = await organisationSchema.find({});
+    res.render('home', { organisations });
+    //res.render('orgs');
+}
+
+
+module.exports.showOrganisation = async (req, res) => {
+    const organisation = await organisationSchema.findById(req.params.id);
+    console.log("/id route", organisation);
+    if (!organisation) {
+        return res.redirect('/orgs');
+    }
+    res.render('organizations/index', { organisation });
+} 
